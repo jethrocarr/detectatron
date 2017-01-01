8c31a07a02d8f51b32434ef6eb757c749afd7785
Jethro Carr <jethro.carr@jethrocarr.com>
2017-01-01 21:54:31 +1300

    Boilerplate from Spring Initalizr

---
+++ src/main/java/com/jethrocarr/DetectatronApplication.java
@@ -0,0 +1,12 @@
+package com.jethrocarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectatronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectatronApplication.class, args);
+	}
+}
